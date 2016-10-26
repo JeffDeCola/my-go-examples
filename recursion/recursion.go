d689378f90b94bfb4a5e508812d2f5cf5e8c49e7
Jeff DeCola (jeff-VirtualBox) <jeff@keeperlabs.com>
2016-10-25 20:26:47 -0700

    added

---
+++ recursion/recursion.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func fibonacci(n int) int {
+	switch {
+	case n == 0:
+		return 0
+	case n == 1:
+		return 1
+	case n <= 40:
+		fibn := fibonacci(n-1) + fibonacci(n-2)
+		return fibn
+	default:
+		return 9999
+	}
+}
+
+func main() {
+	var n int
+	fmt.Scanf("%d\n", &n)
+	fmt.Println(fibonacci(n))
+}
