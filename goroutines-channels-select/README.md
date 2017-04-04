b296b7aa19dcd6f63604633316054709657bc83e
Jeff DeCola (stimpy) <jeff@keeperlabs.com>
2017-04-03 17:14:49 -0700

    init

---
+++ goroutines-channels-select/README.md
@@ -0,0 +1,9 @@
+# goroutines-channels-select example
+
+`goroutines-channels-select` _is a example of using goroutines in go._
+
+[GitHub Webpage](https://jeffdecola.github.io/my-go-examples/)
+
+## GOROUTINES
+
+TBD
---
+++ goroutines-channels-select/goroutines.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+type Hello struct {
+}
+
+// Say says something in miliseconds (ticktime)
+func (h Hello) Say(ch chan string, ticktime int) error {
+
+	ticker := time.Tick(time.Millisecond * time.Duration(ticktime))
+
+	// Loop forever - Long running
+	for {
+		select {
+
+		case now := <-ticker:
+
+			saythis := <-ch
+
+			fmt.Printf("%v %s\n", now, saythis)
+
+			/* case <-quit:
+
+			fmt.Println("Bye")
+			return nil */
+		}
+	}
+	return nil
+}
+
+func main() {
+
+	hi := Hello{}
+	ch1 := make(chan string)
+	ch2 := make(chan string)
+
+	go hi.Say(ch1, 1000)
+	go hi.Say(ch2, 500)
+
+	time.Sleep(5 * time.Second)
+	ch1 <- "Jeff"
+	ch2 <- "Clif"
+
+	time.Sleep(15 * time.Second)
+	ch1 <- "Jack"
+	ch2 <- "Jill"
+
+	time.Sleep(30 * time.Second)
+
+}
