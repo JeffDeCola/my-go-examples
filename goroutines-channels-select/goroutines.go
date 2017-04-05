881797d5cd3f590f6251c0b51193e3b1e4955bb2
Jeff DeCola (Pex-Mac) <jeff@pex.com>
2017-04-04 18:24:58 -0700

    added concurrency

--- goroutines-channels-select/goroutines.go
+++ goroutines-channels-select/goroutines.go
@@ -6,39 +6,44 @@ import (
 )
 
 type Hello struct {
+	defaultticktime int
+	defaultsay      string
 }
 
-func (h Hello) Say(ch chan string, ticktime int) error {
+func (h Hello) Say(ch chan string) {
+
+	ticktime := h.defaultticktime
+	saythis := h.defaultsay
 
 	ticker := time.Tick(time.Millisecond * time.Duration(ticktime))
+	count := 0
 
 	// Loop forever - Long running
 	for {
 		select {
-
 		case now := <-ticker:
-
-			saythis := <-ch
-
-			fmt.Printf("%v %s\n", now, saythis)
-
-			/* case <-quit:
-
-			fmt.Println("Bye")
-			return nil */
+			if saythis == "quit" {
+				break
+			}
+			fmt.Printf("%v %d %s\n", now, count, saythis)
+		case bleh := <-ch:
+			saythis = bleh
 		}
+		count++
 	}
-	return nil
 }
 
 func main() {
 
-	hi := Hello{}
+	hi1 := Hello{defaultticktime: 1000, defaultsay: "Waiting1"}
+	hi2 := Hello{defaultticktime: 500, defaultsay: "Waiting2"}
+
 	ch1 := make(chan string)
 	ch2 := make(chan string)
 
-	go hi.Say(ch1, 1000)
-	go hi.Say(ch2, 500)
+	// A goroutine calling a method with a channel for communication
+	go hi1.Say(ch1)
+	go hi2.Say(ch2)
 
 	time.Sleep(5 * time.Second)
 	ch1 <- "Jeff"
@@ -48,6 +53,8 @@ func main() {
 	ch1 <- "Jack"
 	ch2 <- "Jill"
 
-	time.Sleep(29 * time.Second)
-
+	time.Sleep(4 * time.Second)
+	ch1 <- "quit"
+	time.Sleep(4 * time.Second)
+	ch2 <- "quit"
 }
