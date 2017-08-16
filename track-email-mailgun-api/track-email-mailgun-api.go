ff7552f7d17454a5a7c98a57c92d8a8940bb191a
Jeff DeCola (Pex-Mac) <jeff@pex.com>
2017-08-16 13:34:40 -0700

    bump

--- track-email-mailgun-api/track-email-mailgun-api.go
+++ track-email-mailgun-api/track-email-mailgun-api.go
@@ -4,6 +4,19 @@ import (
 	"fmt"
 )
 
+func SendSimpleMessage(domain, apiKey string) (string, error) {
+	mg := mailgun.NewMailgun(domain, apiKey, publicApiKey)
+	m := mg.NewMessage(
+		"Excited User <mailgun@YOUR_DOMAIN_NAME>",
+		"Hello",
+		"Testing some Mailgun awesomeness!",
+		"YOU@YOUR_DOMAIN_NAME",
+	)
+	_, id, err := mg.Send(m)
+	return id, err
+}
+
 func main() {
-	fmt.Println("hi")
+	fmt.Println("SEnding email to mailgun")
+	return, _ := SendSimpleMessage()
 }
