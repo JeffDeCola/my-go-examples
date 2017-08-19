15bfc8f70ccf6dc7adab93f78c92dc43a7571f0b
Jeff DeCola (Pex-Mac) <jeff@pex.com>
2017-08-18 17:38:18 -0700

    bump mailgun

--- track-email-mailgun-api/track-email-mailgun-api.go
+++ track-email-mailgun-api/track-email-mailgun-api.go
@@ -8,7 +8,11 @@ import (
 	"os"
 	"time"
 
-	"gopkg.in/mailgun/mailgun-go.v1"
+	mailgun "github.com/mailgun/mailgun-go"
+)
+
+const (
+	replyAPIURL = "https://sw.api.mailgun.net/v3"
 )
 
 // Secrets is the client secrets .json file
@@ -38,6 +42,7 @@ func getSecrets() (string, string, string, string) {
 
 // MESSAGE API
 func sendMessage(domain, APIKey, publicAPIKey, fromEmail, sendtoEmail, subject, body string) (string, string) {
+	fmt.Printf("In sendMessage()\n")
 	mg := mailgun.NewMailgun(domain, APIKey, publicAPIKey)
 	message := mg.NewMessage(
 		fromEmail,
@@ -47,7 +52,7 @@ func sendMessage(domain, APIKey, publicAPIKey, fromEmail, sendtoEmail, subject, 
 	)
 	// message.AddCC("baz@example.com")
 	// message.AddBCC("bar@example.com")
-	message.SetTracking(true)
+	//message.SetTracking(true)
 	resp, id, err := mg.Send(message)
 	if err != nil {
 		log.Fatal(err)
@@ -55,24 +60,99 @@ func sendMessage(domain, APIKey, publicAPIKey, fromEmail, sendtoEmail, subject, 
 	return id, resp
 }
 
+// MESSAGE API
+func getStoredMessage(domain, APIKey, publicAPIKey, id string) mailgun.StoredMessage {
+	mg := mailgun.NewMailgun(domain, APIKey, publicAPIKey)
+	mg.SetAPIBase(replyAPIURL)
+
+	resp, err := mg.GetStoredMessage(id)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return resp
+}
+
+func testGetStoredMessage(domain string, APIKey string, publicAPIKey string, id string) {
+	os.Setenv("MG_API_KEY", APIKey)
+	os.Setenv("MG_DOMAIN", domain)
+	os.Setenv("MG_PUBLIC_API_KEY", publicAPIKey)
+	os.Setenv("MG_URL", replyAPIURL)
+
+	mg, err := mailgun.NewMailgunFromEnv()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	// id, err = findStoredMessageID(mg)
+	// if err != nil {
+	//	log.Fatal(err)
+	//	return
+	//}
+
+	// First, get our stored message.
+	msg, err := mg.GetStoredMessage("asdf")
+	fmt.Printf("msg: %+v\n", msg)
+
+	fields := map[string]string{
+		"       From": msg.From,
+		"     Sender": msg.Sender,
+		"    Subject": msg.Subject,
+		"Attachments": fmt.Sprintf("%d", len(msg.Attachments)),
+		"    Headers": fmt.Sprintf("%d", len(msg.MessageHeaders)),
+	}
+	for k, v := range fields {
+		fmt.Printf("%13s: %s\n", k, v)
+	}
+
+	// We're done with it; now delete it.
+	mg.DeleteStoredMessage(id)
+
+}
+
+// Tries to locate the first stored event type, returning the associated stored message key.
+/*func findStoredMessageID(mg mailgun.Mailgun) (string, error) {
+	ei := mg.NewEventIterator()
+	err := ei.GetFirstPage(mailgun.GetEventsOptions{})
+	for {
+		if err != nil {
+			fmt.Println("ugggggg")
+			return "", err
+		}
+		if len(ei.Events()) == 0 {
+			break
+		}
+		for _, event := range ei.Events() {
+			if event["event"] == "stored" {
+				s := event["storage"].(map[string]interface{})
+				k := s["key"]
+				return k.(string), nil
+			}
+		}
+		err = ei.GetNext()
+	}
+	return "", fmt.Errorf("No stored messages found.  Try changing MG_EMAIL_TO to an address that stores messages and try again.")
+}*/
+
 // EVENTS API
 func getMailLog(domain, apiKey, id string) ([]mailgun.Event, error) {
 	mg := mailgun.NewMailgun(domain, apiKey, "")
 	ei := mg.NewEventIterator()
+	begin := time.Now().Add(-150 * time.Minute)
 	err := ei.GetFirstPage(mailgun.GetEventsOptions{
-		Begin:          time.Now().Add(-50 * time.Minute),
+		Begin:          &begin,
 		ForceAscending: true,
 		Limit:          3,
 		Filter: map[string]string{
 			// "message-id": id,
 			// "event": "rejected OR failed",
-			// "event": "accepted",
-			"recipient": "NAME@gmail.com",
+			"event": "stored",
+			//"recipient": "NAME@gmail.com",
 		}})
 	if err != nil {
 		return nil, err
 	}
-	return ei.Events(), nil
+	return ei.Events, nil
 }
 
 func createRoute(domain, fromEmailSubDomain, apiKey string) (mailgun.Route, error) {
@@ -97,8 +177,8 @@ func listRoutes(domain, apiKey string) (int, []mailgun.Route, error) {
 func main() {
 
 	APIKey, publicAPIKey, fromEmailSubDomain, sendtoEmail := getSecrets()
-	fmt.Printf("Secrets are %s, %s, %s %s", APIKey, publicAPIKey, fromEmailSubDomain, sendtoEmail)
-	uniqueID := "1245A"
+	fmt.Printf("Secrets are %s, %s, %s %s\n", APIKey, publicAPIKey, fromEmailSubDomain, sendtoEmail)
+	uniqueID := "777"
 
 	// SENDING EMAIL **********************************************************************************
 	fmt.Printf("Sending email to mailgun using unique ID %s\n", uniqueID)
@@ -109,14 +189,24 @@ func main() {
 	fmt.Printf("Response:\nID: %s\nRESP: %s\n", id, resp)
 
 	// CHECK LOG **************************************************************************************
-	fmt.Printf("Check Log\n")
-	respLog, err := getMailLog(fromEmailSubDomain, APIKey, id)
-	if err != nil {
-		log.Fatal(err)
-	}
-	b, err := json.MarshalIndent(respLog, "", "  ")
-	fmt.Printf("Response:\nRESP: %s\n", b)
-	// os.Stdout.Write(b)
+	/*
+		fmt.Printf("Check Log\n")
+		id := ""
+		respLog, err := getMailLog(fromEmailSubDomain, APIKey, id)
+		if err != nil {
+			log.Fatal(err)
+		}
+		b, err := json.MarshalIndent(respLog, "", "  ")
+		fmt.Printf("Response:\nRESP: %s\n", b)
+		os.Stdout.Write(b)
+	*/
+
+	// GET STORED MESSAGE **************************************************************************************
+	// fmt.Printf("Get Stored Message\n")
+	// id := ""
+	// respStored := getStoredMessage(fromEmailSubDomain, APIKey, publicAPIKey, id)
+	// fmt.Printf("Response:\nRESP: %v\n", respStored)
+	//testGetStoredMessage(fromEmailSubDomain, APIKey, publicAPIKey, id)
 
 	// CREATE ROUTE **************************************************************************************
 	// This code not needed - Easier to do this online
