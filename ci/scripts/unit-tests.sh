07bca8df2cb24630411f48fad192abc26e8d41ee
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeffdecola@gmail.com>
2018-09-02 17:43:56 -0700

    minor

--- ci/scripts/unit-tests.sh
+++ ci/scripts/unit-tests.sh
@@ -25,7 +25,7 @@ ls -lat
 # RUN unit_tests and it shows the percentage coverage
 # print to stdout and file using tee
 # go test -cover ./... | tee test_coverage.txt
-echo "Placeholder to run python tests for my-go-examples" | tee test_coverage.txt
+echo "Placeholder to run go tests for my-go-examples" | tee test_coverage.txt
 
 # Add some whitespace to the begining of each line
 sed -i -e 's/^/     /' test_coverage.txt
