f8d52c7ac5411e16ce81da0b1cdcd11d9c1890ed
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2019-08-11 21:54:08 -0700

    updating goroutine multi-core

--- goroutines/goroutines-multi-core/goroutines-multi-core.go
+++ goroutines/goroutines-multi-core/goroutines-multi-core.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"runtime"
+	"sync"
 
 	"time"
 )
 
 // WORKERS AND CORES
-const numberWorkers = 20
-const numberCores = 3
+const useGoroutine = true
+const numberWorkers = 10
+const timeWork = 1
+const numberCores = 2
 
 // OTHER
-var channelBufferSize = 1 // How many channel buffers
+var channelBufferSize = numberWorkers + 1 // How many channel buffers
 
 // struct to pass in channel
 type workerStats struct {
@@ -19,10 +23,10 @@ type workerStats struct {
 	timeTook time.Duration
 }
 
-func doWork(msgCh chan *workerStats, id int) {
+func doWork(msgCh chan *workerStats, wg *sync.WaitGroup, id int) {
 
 	timeStart := time.Now().UTC()
-	time.Sleep(3 * time.Second)
+	time.Sleep(timeWork * time.Second)
 	timeEnd := time.Now().UTC()
 	timeTook := timeEnd.Sub(timeStart)
 
@@ -30,32 +34,57 @@ func doWork(msgCh chan *workerStats, id int) {
 		id:       id,
 		timeTook: timeTook,
 	}
+	wg.Done()
 
 }
 
-func getStats(msgCh chan *workerStats) {
+func getStats(msgCh chan *workerStats, wg *sync.WaitGroup, numberWorkers int) {
+
+	// WAIT FOR ALL WORKERS TO FINISH
+	fmt.Println("Waiting for all the workers to finish")
+	fmt.Println("There are currently this many goroutines", runtime.NumGoroutine())
+	wg.Wait()
+
 	// Print out stats from each worker
-	for r := range msgCh {
+	for i := 0; i < numberWorkers; i++ {
+		r := <-msgCh
 		fmt.Printf("From Worker %v, Took %v\n", r.id, r.timeTook)
 	}
+
 }
 
 func main() {
+
 	// START
+	start := time.Now()
+
+	// Create wait group
+	var wg sync.WaitGroup
+
+	// SET CORES
+	runtime.GOMAXPROCS(numberCores)
+
 	fmt.Println("Main start time", time.Since(start))
+
+	// How many core amd i using
+	fmt.Println("I have this many cores to use: ", runtime.NumCPU())
 
 	// Make Channel Buffer
 	msgCh := make(chan *workerStats, channelBufferSize)
 
 	// Create Workers on Core
 	for id := 1; id < numberWorkers+1; id++ {
+		wg.Add(1)
 		fmt.Println("Starting worker", id)
-		go doWork(msgCh, id)
-
+		if useGoroutine {
+			go doWork(msgCh, &wg, id)
+		} else {
+			doWork(msgCh, &wg, id)
+		}
 	}
 
 	// Gather Stats from workers
-	go getStats(msgCh)
+	getStats(msgCh, &wg, numberWorkers)
 
 	// END
 	fmt.Println("Main end time", time.Since(start))
