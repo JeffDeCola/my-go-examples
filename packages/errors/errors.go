8fe4cbe6b7ec8c4751283bd55830bdfac68eaa72
Jeff DeCola (TK1-PC Bash on Ubuntu on Windows) <jeff@keeperlabs.com>
2019-08-14 06:24:57 -0700

    error handling

--- packages/errors/errors.go
+++ packages/errors/errors.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/pkg/errors"
+)
+
+func a(x int) (int, error) {
+	if x == 42 {
+		// Make your error
+		return -1, errors.New("can't work with 42")
+	}
+	return x + 3, nil
+}
+
+func b(i int) (int, error) {
+
+	// Do stuff
+
+	// Call another function
+	return a(i)
+}
+
+func checkErr(err error) {
+	if err != nil {
+		fmt.Printf("Error is %+v\n", err)
+		log.Fatal("ERROR:", err)
+	}
+}
+
+func main() {
+
+	//MULTIPLE RETURN VALUES
+	r, err := b(43)
+	if err != nil {
+		// Handle the error
+		fmt.Printf("Error is %+v\n", err)
+		log.Fatal("ERROR:", err)
+	}
+	// Continue
+	fmt.Println("Returned", r)
+
+	//MULTIPLE RETURN VALUES (Simpler form)
+	r, err = b(42)
+	checkErr(err)
+	fmt.Println("Returned", r)
+}
