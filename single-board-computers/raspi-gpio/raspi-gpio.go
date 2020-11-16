a7c3b6bc242361dbb6369399deb593a8f7282561
Jeff DeCola (Jeffs-Raspi-4G8B) <jeffdecola@gmail.com>
2020-11-16 01:35:07 -0800

    update

--- single-board-computers/raspi-gpio/raspi-gpio.go
+++ single-board-computers/raspi-gpio/raspi-gpio.go
@@ -1,75 +1,47 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"os"
-	"time"
-	"github.com/stianeikeland/go-rpio"
+    "fmt"
+    "log"
+    "time"
+
     "periph.io/x/periph/conn/gpio"
     "periph.io/x/periph/host"
     "periph.io/x/periph/host/rpi"
-
-	errors "github.com/pkg/errors"
-	log "github.com/sirupsen/logrus"
 )
 
-const toolVersion = "1.0.0"
-
-// A Function that also returns an error
-func jeffFunc() (string, error) {
-
-	fmt.Println("opening gpio")
-
-    host.Init()
-    t := time.NewTicker(500 * time.Millisecond)
-    for l := gpio.Low; ; l = !l {
-        rpi.P1_33.Out(l)
-        <-t.C
+func main() {
+    // Load all the drivers:
+    if _, err := host.Init(); err != nil {
+        log.Fatal(err)
     }
 
-	return "Everything worked great", nil
+    // Lookup a pin by its number:
+    p20 := gpioreg.ByName("GPI20")
+    if p20 == nil {
+        log.Fatal("Failed to find GPI20")
+    }
+
+    fmt.Printf("%s: %s\n", p20, p20.Function())
+
+    // Set it as input, with an internal pull down resistor:
+    if err = p20.In(gpio.PullDown, gpio.BothEdges); err != nil {
+        log.Fatal(err)
+    }
+
+    // Wait for edges as detected by the hardware, and print the value read:
+    for {
+        p20.WaitForEdge(-1)
+        fmt.Printf("-> %s\n", p20.Read())
+    }
+
+    t := time.NewTicker(500 * time.Millisecond)
+    for l := gpio.Low; ; l = !l {
+        // Lookup a pin by its location on the board:
+        if err := rpi.P1_40.Out(l); err != nil {
+            log.Fatal(err)
+        }
+        <-t.C
+    }
 }
 
-// Check your error
-func checkErr(err error) {
-	if err != nil {
-		fmt.Printf("Error is %+v\n", err)
-		log.Fatal("ERROR:", err)
-	}
-}
-
-func init() {
-
-	// SET LOG LEVEL
-	// log.SetLevel(log.InfoLevel)
-	log.SetLevel(log.TraceLevel)
-
-	// SET FORMAT
-	log.SetFormatter(&log.TextFormatter{})
-	// log.SetFormatter(&log.JSONFormatter{})
-
-	// SET OUTPUT (DEFAULT stderr)
-	log.SetOutput(os.Stdout)
-
-	// VERSION FLAG
-	version := flag.Bool("v", false, "prints current version")
-	// Parse the flags
-	flag.Parse()
-
-	// CHECK VERSION
-	if *version {
-		fmt.Println(toolVersion)
-		os.Exit(0)
-	}
-
-}
-
-func main() {
-
-	log.Trace("Calling jeffFunc to get result")
-	r, err := jeffFunc()
-	checkErr(err)
-	fmt.Println("Returned", r)
-
-}
