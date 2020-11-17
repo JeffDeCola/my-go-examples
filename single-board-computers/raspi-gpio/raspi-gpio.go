07c5d5e7bcd6334372237375eccac67ab77b2c2b
Jeff DeCola (Jeffs-Raspi-4G8B) <jeffdecola@gmail.com>
2020-11-17 03:53:12 -0800

    update raspi gpio

--- single-board-computers/raspi-gpio/raspi-gpio.go
+++ single-board-computers/raspi-gpio/raspi-gpio.go
@@ -1,47 +1,69 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "time"
+	"fmt"
+	"log"
 
-    "periph.io/x/periph/conn/gpio"
-    "periph.io/x/periph/host"
-    "periph.io/x/periph/host/rpi"
+	"periph.io/x/periph/conn/gpio"
+	"periph.io/x/periph/conn/gpio/gpioreg"
+	"periph.io/x/periph/host"
 )
 
 func main() {
-    // Load all the drivers:
-    if _, err := host.Init(); err != nil {
-        log.Fatal(err)
-    }
 
-    // Lookup a pin by its number:
-    p20 := gpioreg.ByName("GPI20")
-    if p20 == nil {
-        log.Fatal("Failed to find GPI20")
-    }
+	// Load all the drivers: Initialize the periph host
+	_, err := host.Init()
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    fmt.Printf("%s: %s\n", p20, p20.Function())
+	// Lookup pins by their number
+	button_in := gpioreg.ByName("GPIO20")
+	if button_in == nil {
+		log.Fatal("Failed to find GPI20")
+	}
+	led_out := gpioreg.ByName("GPIO21")
+	if led_out == nil {
+		log.Fatal("Failed to find GPI21")
+	}
 
-    // Set it as input, with an internal pull down resistor:
-    if err = p20.In(gpio.PullDown, gpio.BothEdges); err != nil {
-        log.Fatal(err)
-    }
+	// Setup P20 as INPUT
+	// the RasPi has an internal pull down resistor
+	// PULL DOWN - DEFAULTS TO LOW WHEN UNCONNECTED
+	// RISING EDGE - LOW TO HIGH
+	err = button_in.In(gpio.PullDown, gpio.RisingEdge)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Setup P21 as OUTPUT
+	// With output level 1 (true)
+	err = led_out.Out(true)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-    // Wait for edges as detected by the hardware, and print the value read:
-    for {
-        p20.WaitForEdge(-1)
-        fmt.Printf("-> %s\n", p20.Read())
-    }
+	toggle := gpio.Low
+	count := 0
 
-    t := time.NewTicker(500 * time.Millisecond)
-    for l := gpio.Low; ; l = !l {
-        // Lookup a pin by its location on the board:
-        if err := rpi.P1_40.Out(l); err != nil {
-            log.Fatal(err)
-        }
-        <-t.C
-    }
+	for {
+
+		fmt.Printf("  Pin 20 (BUTTON) is %s: %s (%s)\n", button_in, button_in.Function(), button_in.Read())
+		fmt.Printf("  Pin 21 (LED)    is %s: %s (%s)\n", led_out, led_out.Function(), led_out.Read())
+
+		// WAIT FOR BUTTON PRESS - RISING EDGE (LOW TO HIGH) - THIS IS BLOCKING
+		// -1 disables the timeout
+		fmt.Printf("%v Waiting for button press (LOW TO HIGH)...\n", count)
+		button_in.WaitForEdge(-1)
+
+		// TOOGLE THE LED
+		err := led_out.Out(toggle)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// Change gpio.Low to gpio.High
+		toggle = !toggle
+		count++
+
+	}
 }
-
