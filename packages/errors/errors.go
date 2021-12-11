ac8c8fd08865651347c86bda7e5250edf7c1a927
Jeff DeCola (TK2-PC) <jeffdecola@gmail.com>
2021-12-11 11:08:14 -0800

    udpated errors

--- packages/errors/errors.go
+++ packages/errors/errors.go
@@ -3,72 +3,92 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"strconv"
 
-	errors "github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 )
 
-func a(i int) (int, error) {
+func a() (string, error) {
 
-	// Do stuff
+	// DO STUFF
 
-	// Call another function
-	o, err := b(i)
-
-	return o, err
-
-}
-
-func b(i int) (int, error) {
-
-	// Do stuff
-
-	// Call another function
-	o, err := c(i)
-
-	return o, err
-
-}
-
-func c(i int) (int, error) {
-
-	// Do stuff
-	fmt.Print("What is 2 + 2?")
-	_, err := fmt.Scan(&answer)
+	// CALL ANOTHER FUNCTION
+	out, err := b()
 	if err != nil {
-		return "", fmt.Errorf("unable to open paraphrase: %w", err)
+		return out, fmt.Errorf("error in a: %w", err)
 	}
 
-	if i == 42 {
+	return out, err
+
+}
+
+func b() (string, error) {
+
+	// DO STUFF
+
+	// CALL ANOTHER FUNCTION
+	out, err := c()
+	if err != nil {
+		return out, fmt.Errorf("error in b: %w", err)
+	}
+
+	return out, err
+
+}
+
+func c() (string, error) {
+
+	var userAnswer string
+
+	// GET USER ANSWER
+	fmt.Print("What is 2 + 2?: ")
+	_, err := fmt.Scan(&userAnswer)
+	if err != nil {
+		return "", fmt.Errorf("unable to get user input: %w", err)
+	}
+
+	if userAnswer == "stop" {
+		return userAnswer, nil
+	}
+
+	// CONVERT TO INT
+	answerInt, err := strconv.Atoi(userAnswer)
+	if err != nil {
+		return userAnswer, fmt.Errorf("unable to convert string: %w", err)
+	}
+
+	// IS ANSWER CORRECT?
+	if answerInt != 4 {
 		// Make your error
-		err := errors.New("can't work with 42")
-		return 22, err
+		err := errors.New("incorrect answer " + userAnswer + "error in c")
+		return userAnswer, err
 	}
 
-	return i + 3, nil
+	return userAnswer, nil
 
 }
 
 func main() {
 
-	//MULTIPLE RETURN VALUES
-	r, err := a(43)
-	if err != nil {
-		// Handle the error
-		log.Fatalf("Error getting paraphrase: %s", err)
-		// log.Fatal("ERROR:", err)
-	}
-	// Continue
-	fmt.Println("Returned", r)
+	// ASK USER A MATH QUESTION UNTIL THEY TYPE "stop"
+	for {
 
-	//MULTIPLE RETURN VALUES (Simpler form)
-	r, err = a(42)
-	if err != nil {
-		fmt.Printf("Error is %+v\n", err)
-		log.Fatalf("Error getting paraphrase: %s", err)
+		userAnswer, err := a()
 
-		log.Fatal("ERROR:", err)
+		if err != nil {
+			log.Errorf("INCORRECT: %s", err)
+		} else {
+			if userAnswer == "stop" {
+				fmt.Println("Done")
+				break
+			} else {
+				fmt.Println("CORRECT: ", userAnswer)
+			}
+		}
+
+		fmt.Println("-----------------------------")
 	}
-	fmt.Println("Returned", r)
+
 }
